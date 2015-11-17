f9ea583aa696ef3bec686fe32bd76f108b2e52e8
roxana <popescu.roxana.andreea@gmail.com>
2015-11-17 23:49:27 +0200

    Added filtering, default sorting

--- app/angular/controller/MusicStoreController.js
+++ app/angular/controller/MusicStoreController.js
@@ -13,10 +13,30 @@
         }, {
             name: 'Song name'
         }, {
-            name: 'Like'
+            name: 'Add to favorites'
         }];
 
+        var musicStoreresponse = [
+            {
+                artistName: 'Beatles',
+                songName: 'Drive my car'
+            },
+            {
+                artistName: 'John Lennon',
+                songName: 'Imagine'
+            },
+            {
+                artistName: 'Coldplay',
+                songName: 'Fix you'
+            },
+            {
+                artistName: 'Rammstein',
+                songName: 'Amour'
+            }
+        ];
+
         $scope.headers = headers;
+        $scope.musicStore = musicStoreresponse;
     };
 
     app.controller('MusicStoreController', ['$scope', MusicStoreController]);
